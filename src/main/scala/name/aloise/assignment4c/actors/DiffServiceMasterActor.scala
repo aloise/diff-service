683feaf53bcc24c6121c9405f9ea043c2f590cb6
aloise <aloise@aloise.name>
2016-05-18 22:29:20 +0300

    [+] basic server - test routes

---
+++ src/main/scala/name/aloise/assignment4c/actors/DiffServiceMasterActor.scala
@@ -0,0 +1,16 @@
+package name.aloise.assignment4c.actors
+
+import akka.actor.Actor
+import akka.actor.Actor.Receive
+
+/**
+  * User: aloise
+  * Date: 18.05.16
+  * Time: 20:38
+  */
+class DiffServiceMasterActor extends Actor {
+  override def receive: Receive = {
+    case _ =>
+
+  }
+}
--- src/main/scala/name/aloise/assignment4c/server/DiffService.scala
+++ src/main/scala/name/aloise/assignment4c/server/DiffService.scala
@@ -1,11 +1,15 @@
 package name.aloise.assignment4c.server
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorSystem, Props}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.Http.ServerBinding
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
-import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.Uri.Path._
+import akka.http.scaladsl.model.Uri.Path
 import akka.stream.ActorMaterializer
+import name.aloise.assignment4c.actors.DiffServiceMasterActor
+
 import scala.concurrent.Future
 
 /**
@@ -13,29 +17,40 @@ import scala.concurrent.Future
   * Date: 18.05.16
   * Time: 19:57
   */
-class DiffService( bindAddress:String, bindPort:Int ) {
+class DiffService( bindAddress:String, bindPort:Int, serviceVersion:Int = 1 ) {
 
   implicit val system = ActorSystem("diff-service-system")
+
   implicit val materializer = ActorMaterializer()
+
   // needed for the future flatMap/onComplete in the end
   implicit val executionContext = system.dispatcher
   implicit var serverBindingFuture:Option[Future[ServerBinding]] = None
 
+  val processingSystem = ActorSystem("diff-processing-service-system")
+  val diffServiceMasterActor = processingSystem.actorOf( Props( classOf[DiffServiceMasterActor] ) )
 
-  protected val route =
-    path("hello") {
-      get {
-        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
-      }
-    } ~
-    path("") { // index path
-      get {
-        complete( greeting )
-      }
-    }
+  val xxxx: Path = Path./( "v2" )
+
+  protected val requestHandler: HttpRequest => HttpResponse = {
+    case HttpRequest( GET, Uri.Path("/"), _, _, _) =>
+      HttpResponse( entity = greeting )
+
+    case HttpRequest( GET, Uri( _, _, `xxxx`, _, _ ), _, _, _ ) =>
+      HttpResponse( entity = "test - v2 - " + 1 )
+
+    case HttpRequest( GET, Uri( _, _, Slash( Segment( "v1", Slash( Segment( "diff", Slash( Segment( str, Slash( Segment ( "left", Uri.Path.Empty ) ) ) ) ) ) ) ), _, _ ) , _, _, _ ) =>
+      HttpResponse( entity = "test - left - " + str )
+
+    case HttpRequest( GET, Uri( _, _, Slash( Segment( "v1", Slash( Segment( "diff", Slash( Segment( str, Slash( Segment ( "right", Uri.Path.Empty ) ) ) ) ) ) ) ), _, _ ) , _, _, _ ) =>
+      HttpResponse( entity = "test - right - " + str )
+
+    case _: HttpRequest =>
+      HttpResponse(404, entity = "Unknown resource!")
+  }
 
   def start():Future[ServerBinding] = {
-    val bindingFuture = Http().bindAndHandle(route, bindAddress, bindPort)
+    val bindingFuture = Http().bindAndHandleSync(requestHandler, bindAddress, bindPort) // Http().bindAndHandle(routes, bindAddress, bindPort)
     serverBindingFuture = Some( bindingFuture )
     bindingFuture
   }
@@ -53,11 +68,7 @@ class DiffService( bindAddress:String, bindPort:Int ) {
   def greeting =
     HttpEntity(
       ContentTypes.`text/html(UTF-8)`,
-      """<!doctype html>
-        |<html lang=en>
-        |<head><meta charset=utf-8><title>Diff Service</title></head>
-        |<body><p>Diff Sevice</p></body>
-        |</html>""".stripMargin
+      s"<!doctype html><html lang=en><head><meta charset=utf-8><title>Diff Service</title></head><body><p>Diff Service v$serviceVersion</p></body></html>"
     )
 
 }
